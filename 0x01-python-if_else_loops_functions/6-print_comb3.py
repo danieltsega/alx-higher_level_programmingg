2e1470c5fecdb10a3e8fd40df932a1554215a829
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-07 05:59:49 -0800

    [Feature]: Added a python script that prints a combination of numbers with some exceptions

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(0, 10):
+        if i >= j:
+            continue
+        elif i < j:
+            print("{}{}, ".format(i, j), end="")
+print("{}{}".format(i, j))
