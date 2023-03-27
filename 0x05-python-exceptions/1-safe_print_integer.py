46332b8bd3742b9e231a328f8989ed3949f48b0f
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-27 13:33:49 -0700

    [Fix]: Added another except functionality to the function

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,9 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        return True
     except ValueError:
         return False
-    else:
-        return True
+    except:
+        return False
 
