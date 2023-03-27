5c190ebf67ff6dca718734c5987913c5f1443c8b
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-27 13:31:56 -0700

    [Update]: Adding a return fucntion

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except ValueError:
-        value = False
+        return False
     else:
-        value = True
+        return True
 
-    return value
