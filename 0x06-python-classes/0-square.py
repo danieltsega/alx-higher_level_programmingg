bb395d29f50aabf53ac47278240e0eac01730349
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-28 11:33:26 -0700

    [Update]: Updated the comment

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
 class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'
        Class that defines a square and does nothing
