226545cf0abaa41c011e7a2b24dba9ed9de1815d
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-28 12:49:56 -0700

    [Feature]: Adding a square class that that has an init method that declares private instance variable size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defining a Class"""
+
+
+class Square:
+    """A Class that defines a square and its attributes
+    """
+
+    def __init__(self, size):
+        """An initinalization method that initializes
+        a private instance variable
+        """
+        self.__size = size
