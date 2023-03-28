8d65dd6fa8a3c1aa93c481aeae01eed8d5ce6dae
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-28 11:44:42 -0700

    [Update]: Updated the comment

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Define a class Square"""
 
+
 class Square:
-    """Class that defines a square and does nothing
-    """
+    """Class that defines a square and does nothing."""
     pass
 
