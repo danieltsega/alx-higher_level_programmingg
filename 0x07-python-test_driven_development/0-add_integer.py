1b47c4170b4196409f24a7b3a52b1077ed705f14
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-04 09:41:55 -0700

    [Fix]: Fixed a whitespace error and casted the arguments to integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,4 +15,6 @@ def add_integer(a, b=98):
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
 
+    a = int(a)
+    b = int(b)
     return a + b
