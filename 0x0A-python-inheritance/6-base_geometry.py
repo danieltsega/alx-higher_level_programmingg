3c45297afae9ded0e3b147837b48c8245bd80ce6
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-10 10:23:21 -0700

    [Feature]: Adding a public method that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A BaseGeometry Module
+"""
+
+
+class BaseGeometry:
+    """A Base Geometry class
+    """
+
+    def area(self):
+        """A public method that raises
+        some exception
+        """
+
+        raise Exception("area() is not implemented")
