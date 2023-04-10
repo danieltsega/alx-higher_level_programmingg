923b430578f615579bb8630b65866af6a4999225
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-10 15:55:41 -0700

    [Feature]: Adding a function that adds an attribute if the type of the object has an attribute of a dictionary

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A Attribute Module
+"""
+
+
+def add_attribute(obj, name, value):
+    """A Function that adds an attribute
+    for a dictionary instance
+    """
+
+    if hasattr(obj, "__dict__"):
+        setattr(obj, name , value)
+
+    else:
+        raise TypeError("can't add new attribute")
