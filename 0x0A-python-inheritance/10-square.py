44337c0591ada6d969ca332351c2f0e0e99996e5
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-10 13:49:12 -0700

    [Feature]: Adding a class of square that inherits from a class rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A Square Module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A square class"""
+
+    def __init__(self, size):
+        """An init method"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """A methid that gets area from
+        the parent class
+        """
+
+        return super().area()
