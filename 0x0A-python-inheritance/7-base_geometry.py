f6ed4c6c8630c471617e4b1f0d91cb7fcc92cef6
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-10 11:02:09 -0700

    [Fix]: Fixed a typo error

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,4 +22,4 @@ class BaseGeometry:
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be grater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
