84cf38f1d466a2853b81395f8e963d47854e5929
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-10 10:40:17 -0700

    [Feature]: Adding a function that checke for a value and a type error

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A BaseGeometry Module
+"""
+
+
+class BaseGeometry:
+    """A Base Geometry class
+    """
+
+    def area(self):
+        """A public method that raises
+        some exception
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A public method that validates
+        the value
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be grater than 0".format(name))
