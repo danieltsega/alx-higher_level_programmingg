5a4f1103cd4a5cb6755dd0ae295fb410bcf617d6
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-11 16:20:40 -0700

    [Feature]: Adding a function that changes a class object to a ditcionary type

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class to Dictionary
+Module
+"""
+
+
+def class_to_json(obj):
+    """A function that changes
+    a class toa dictionary
+    format
+    """
+
+    return obj.__dict__
