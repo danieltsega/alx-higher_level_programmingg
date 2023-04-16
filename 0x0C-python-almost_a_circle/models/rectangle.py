69e6e275fedaba5d229a020c35200f35c365d698
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-16 03:40:03 -0700

    [Fix]: Fixed the constructor method attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,10 +14,10 @@ class Rectangle(Base):
         """A constractor method"""
 
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
