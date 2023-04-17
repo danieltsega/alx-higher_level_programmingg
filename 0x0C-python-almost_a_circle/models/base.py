aeb47a0f4c194ac4c3bb320065fc86bc18e828d1
DannySanchez6658 <danieltsega6658@gmail.com>
2023-04-17 06:15:26 -0700

    [Feature]: Adding a static method that coverts a string to json in our base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """ The Base Class Module"""
 
 
+import json
+
 class Base:
     """A base class that is the
     base of all other classes
@@ -17,3 +19,14 @@ class Base:
         else:
            Base.__nb_objects += 1
            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """A method that returns a json
+        representative of a string
+        """
+
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
