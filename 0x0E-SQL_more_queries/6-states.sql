a6294b79d5e76e4a2cc9e57a4065bf7b6d7aab4b
DannySanchez6658 <danieltsega6658@gmail.com>
2023-05-17 21:21:36 +0000

    [Fix]: Fixed the AUTO_INCREMENT syntax

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- A script that creates a database and table
--- A script that creates a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS htbtn_0d_usa.states(
-id INT UNIQUE AUTO INCREMENT NOT NULL PRIMARY KEY,
+id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 name VARCHAR(256) NOT NULL
 );
