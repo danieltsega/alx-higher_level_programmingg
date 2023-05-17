b2a818258fd17047348de2558d594d14467a4706
DannySanchez6658 <danieltsega6658@gmail.com>
2023-05-17 21:27:55 +0000

    [Fix]: Fixed the entire script

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- A script that creates a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS htbtn_0d_usa.states(
-id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(256) NOT NULL
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
 );
