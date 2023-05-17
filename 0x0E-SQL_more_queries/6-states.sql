2ad352111b1dfb863f5f7065cf83cfae88f82899
DannySanchez6658 <danieltsega6658@gmail.com>
2023-05-17 21:19:45 +0000

    [Fix]: Fixed the script by removing the use command and using the '.' way of accessing a database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
 -- A script that creates a database and table
 -- A script that creates a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS htbtn_0d_usa.states(
 id INT UNIQUE AUTO INCREMENT NOT NULL PRIMARY KEY,
 name VARCHAR(256) NOT NULL
 );
