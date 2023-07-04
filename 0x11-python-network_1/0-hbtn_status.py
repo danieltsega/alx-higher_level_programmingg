66e104eca30a9692a0651ff6489ebbf7447c7ae4
DannySanchez6658 <danieltsega6658@gmail.com>
2023-07-04 12:45:29 -0700

    [Feature]: Add Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Module: '0-hbtn_status'
+Python script that fetches https://alx-intranet.hbtn.io/status
+'''
+
+import urllib.request
+
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print('Body response:')
+print(f'\t- type: {type(body)}')
+print(f'\t- content: {body}')
+print(f"\t- utf8 content: {body.decode('utf-8')}")
